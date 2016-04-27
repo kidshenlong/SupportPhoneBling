60495d507ad41bb03c840f5cde0deeffe1b6d886
Michael Patterson-Muir <michael.patterson-muir@bbc.co.uk>
2016-04-27 17:50:00 +0100

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "SupportPhoneBling"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  //groupID % artifactID % revision
+  "com.twilio.sdk" % "twilio-java-sdk" % "3.4.5"
+)
---
+++ src/main/scala/Twilio.scala
@@ -0,0 +1,40 @@
+/**
+ * Created by pattem92 on 27/04/2016.
+ */
+
+import com.twilio.sdk.TwilioRestClient
+import com.twilio.sdk.resource.instance.Account
+import org.apache.http.NameValuePair
+import org.apache.http.message.BasicNameValuePair
+
+//import scala.collection.JavaConverters
+import scala.collection.JavaConversions._
+
+object Twilio {
+
+  val ACCOUNT_SID = ???
+
+  val AUTH_TOKEN = ???
+
+  def main(args: Array[String]) = {
+
+
+
+    val client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+
+    // Get the main account (The one we used to authenticate the client)
+    val mainAccount = client.getAccount
+
+    val messageFactory = mainAccount.getMessageFactory
+
+    val params:List[NameValuePair] = List(
+      new BasicNameValuePair("To", "XXXXX"),
+      new BasicNameValuePair("From", "XXXX"),
+      new BasicNameValuePair("Body", "XXXXX")
+    )
+
+    val sms = messageFactory.create(params)
+
+  }
+
+}
